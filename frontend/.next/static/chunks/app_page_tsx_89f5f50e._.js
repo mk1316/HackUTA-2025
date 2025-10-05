0132004a8ec6a3ca2356852218220604df1820bb
Piyush Singh <144399749+piyushgirisingh@users.noreply.github.com>
2025-10-04 19:31:34 -0500

    Remove frontend/.next/ build files from git tracking

--- frontend/.next/static/chunks/app_page_tsx_89f5f50e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_04626ba2._.js",
-  "static/chunks/node_modules_6231029d._.js"
-],
-    source: "dynamic"
-});
