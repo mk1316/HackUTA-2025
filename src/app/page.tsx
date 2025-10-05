6dbc4932763aff5e878417c0429001fec25f38ce
mk1316 <kfesaha15@gmail.com>
2025-10-05 07:17:25 -0500

    rollback auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,5 @@
-import { auth0 } from "@/lib/auth0";
-import './globals.css';
 import AppHomeClient from './components/AppHomeClient';
 
-export default async function Home() {
-  const session = await auth0.getSession();
-
-  if (!session) {
-    return (
-      <main>
-        <a href="/auth/login?screen_hint=signup">
-          <button>Sign up</button>
-        </a>
-        <a href="/auth/login">
-          <button>Log in</button>
-        </a>
-      </main>
-    );
-  }
-
-  return (
-    <main>
-      <h1>Welcome, {session.user.name}!</h1>
-      <p>
-        <a href="/auth/logout">
-          <button>Log out</button>
-        </a>
-      </p>
-      <AppHomeClient />
-    </main>
-  );
+export default function Home() {
+  return <AppHomeClient />;
 }
